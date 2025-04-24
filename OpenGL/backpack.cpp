a6a4bb9c2eea6fa89717197ce84ba784f34e41e2
Adrien354 <adrian.filipek@outlook.com>
2025-04-24 09:32:53 +0200

    bug fix for macos

--- OpenGL/backpack.cpp
+++ OpenGL/backpack.cpp
@@ -97,15 +97,13 @@ int main()
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		std::cout << 1.0f / deltaTime << std::endl;
-
 		// input
 		// -----
 		processInput(window);
 
 		// render
 		// ------
-		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// don't forget to enable shader before setting uniforms
